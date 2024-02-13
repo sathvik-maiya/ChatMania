c87e60831eb34f36302a222a2f9979fec9d4c328
Sathvik v Maiya <94783587+sathvik-maiya@users.noreply.github.com>
2024-02-14 00:08:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-mania.vercel.app",
+    origin: '*',
     methods: ["GET", "POST"],
     credentials: true 
   },
